9d6504a0cdefe8746253fe6905c82a6d46c82200
Hans <hfvos90@gmail.com>
2018-11-28 16:33:35 -0800

    deleted all comments

--- homework5/homework5p1.py
+++ homework5/homework5p1.py
@@ -14,12 +14,7 @@ def show():
     # print(descr)
     # total = int(split1[1]) + int(split1[2]) + int(split1[3])
     # print(split1[0], split1[1], split1[2], split1[3], total)
-    # adding comments
-    # adding comments again
-    # adding third comment
-    # adding forth comment
-    # adding fifth comment
-    # adding sixth comment
+    
 
 show()
 
