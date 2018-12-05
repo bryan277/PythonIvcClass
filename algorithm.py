05137d5883d031a55bd2c1a1432dc85c9e063c5c
Hans <hfvos90@gmail.com>
2018-12-04 18:46:40 -0800

    added new file

---
+++ algorithm.py
@@ -0,0 +1,38 @@
+# algorithm - a deteced sequence of steps for carrying out some process. A receipe.
+
+            # - Time always more than one algorithm to the same problem
+            # - computer scientist study algorithm and came up with wasy to complete them and style which one better than others
+            # - The most common criterion to compare algorithm is time(step)
+            # - measure magnitude fo running time of algorithm
+            # - the idea of big o is the express how much slower the program as input data get large
+            # - focuses on steps to be excuted
+            # - ignores - differences in language compiled vs interpreted
+
+# loops - loops are multiplieactive in terms of time
+#         nested loops multiply the n amount of time need to run the body, so its longerr time.
+
+
+# sorting algorithm - sort data alphebetically or numerically in order
+                    # ex. Bubblesort has complexity of
+                    #     - you loops through elements in list
+                    #     - compare a adjacent elements
+                    #     - swap values if set of order
+
+
+# To sort a if element list need 9 steps
+
+# Bubblesort is efficient
+# Quicksort - smarter, has complexity of (nlog(n))
+
+# Searching - linear search - search through every single element in list sequentially
+# worse case      end of list (n)
+# average case    n/2
+# best case       first element
+
+# linear search is inefficient
+
+# if we have a long list then linear search is inefficient, use Binary search 0(log(n))
+# 1. the list sorted
+# 2. half the list, compare the middle for a match
+# 3. no match is middle index value greater or less than the keys
+# 4. go the left or right and continue to half
